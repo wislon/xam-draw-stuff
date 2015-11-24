a8ba743dbda561da8b7bf4b39e63c6ec0e0010a9
John Wilson <wislon@gmail.com>
2015-11-25 10:16:31 +1100

    ui - add a simple dictionary-property BaseViewModel for binding stuff

---
+++ DrawAvatars01/DrawAvatars01/ViewModels/BaseViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DrawAvatars01.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private readonly Dictionary<string, object> _values = new Dictionary<string, object>();
+
+        public void ResetProperties()
+        {
+            _values.Clear();
+        }
+
+        public T GetValue<T>([CallerMemberName] string property = null)
+        {
+            if (!string.IsNullOrEmpty(property) && _values.ContainsKey(property)
+                && _values[property] != null && _values[property] is T)
+            {
+                return (T)_values[property];
+            }
+            return default(T);
+        }
+
+        public void SetValue(object value, [CallerMemberName] string property = null)
+        {
+            if (!string.IsNullOrEmpty(property))
+            {
+                _values[property] = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(property));
+                }
+            }
+        }
+
+        public void RefreshValue([CallerMemberName] string property = null)
+        {
+            if (!string.IsNullOrEmpty(property) && PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public virtual void Dispose()
+        {
+            ResetProperties();
+        }
+    }
+}
