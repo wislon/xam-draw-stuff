0c35eabcb867760eb7ef8149a4c3db57b9300a6b
John Wilson <wislon@gmail.com>
2015-11-25 10:56:17 +1100

    ui - updating ImageButtonPage to use BaseContentPage & BaseViewModel. Makes binding stuff easier.

--- DrawAvatars01/DrawAvatars01/ImageButtonPage.xaml.cs
+++ DrawAvatars01/DrawAvatars01/ImageButtonPage.xaml.cs
@@ -1,22 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using DrawAvatars01.ViewModels;
 
 namespace DrawAvatars01
 {
-    public partial class ImageButtonPage : ContentPage
+    public partial class ImageButtonPage : BaseContentPage
     {
         private readonly string _dataPath;
+
+        public ImageButtonPageViewModel ViewModel { get; private set; }
 
         public ImageButtonPage(string dataPath)
         {
             InitializeComponent();
+            
+            SetViewModel(new ImageButtonPageViewModel());
+            ViewModel = GetViewModel<ImageButtonPageViewModel>();
+            ViewModel.Init();
 
             _dataPath = dataPath;
+        }
+
+    }
+
+    public class ImageButtonPageViewModel : BaseViewModel
+    {
+        public string MainText
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+
+        public void Init()
+        {
+            MainText = "Welcome to Page 2";
         }
     }
 }
