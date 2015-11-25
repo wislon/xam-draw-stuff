d437e89199f1be9f89407a746c9c95646a3275ad
John Wilson <wislon@gmail.com>
2015-11-25 15:40:55 +1100

    ios - add DummyImageButton and DummyImageButtonRenderer because XLabs.Forms controls version wasn't being linked.
     - had to add a single static method to reference at platform level, as usual. wtf??

---
+++ DrawAvatars01/DrawAvatars01.iOS/DummyImageButtonRenderer.cs
@@ -0,0 +1,18 @@
+﻿using DrawAvatars01;
+using DrawAvatars01.iOS;
+using Splat;
+using Xamarin.Forms;
+using XLabs.Forms.Controls;
+
+[assembly: ExportRenderer(typeof(DummyImageButton), typeof(DummyImageButtonRenderer))]
+namespace DrawAvatars01.iOS
+{
+    public class DummyImageButtonRenderer : ImageButtonRenderer, IEnableLogger
+    {
+        public static void Initialise()
+        {
+            LogHost.Default.Debug("DummyImageButtonRenderer Initialised");
+        }
+         
+    }
+}
