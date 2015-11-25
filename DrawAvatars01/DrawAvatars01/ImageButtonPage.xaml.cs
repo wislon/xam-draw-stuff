2b21ebb4673d58411ef0931112b6d1020befe15e
John Wilson <wislon@gmail.com>
2015-11-25 15:41:50 +1100

    ui - proof of concept imagebutton. clunky control, needs a lot of work, but it'll do to start

--- DrawAvatars01/DrawAvatars01/ImageButtonPage.xaml.cs
+++ DrawAvatars01/DrawAvatars01/ImageButtonPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using DrawAvatars01.ViewModels;
+﻿
+using DrawAvatars01.ViewModels;
+using Xamarin.Forms;
+using XLabs.Enums;
 
 namespace DrawAvatars01
 {
@@ -11,27 +14,58 @@ namespace DrawAvatars01
         public ImageButtonPage(string dataPath)
         {
             InitializeComponent();
-            
+
             SetViewModel(new ImageButtonPageViewModel());
             ViewModel = GetViewModel<ImageButtonPageViewModel>();
             ViewModel.Init();
 
             _dataPath = dataPath;
+
+            var pickupbtn = new XLabs.Forms.Controls.ImageButton()
+                            {
+                                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                                WidthRequest = 200,
+                                HeightRequest = 54,
+                                ImageHeightRequest = 48,
+                                ImageWidthRequest = 48,
+                                Orientation = ImageOrientation.ImageToLeft,
+                                Text = "John Wilson",
+                                Source = "https://d2ojpxxtu63wzl.cloudfront.net/static/2bacd1af951486806f4f0c452c5549aa_9b5a49c8032bb699fd25f9d8ba090b26aa519e27ab686e66aa51457444be2699",
+                                BorderRadius = 27,
+                                BackgroundColor = Color.Silver,
+                                BorderColor = Color.Silver,
+                                BorderWidth = 2,
+                            };
+            containerLayout.Children.Add(pickupbtn);
+
         }
 
-    }
-
-    public class ImageButtonPageViewModel : BaseViewModel
-    {
-        public string MainText
+        public class ImageButtonPageViewModel : BaseViewModel
         {
-            get { return GetValue<string>(); }
-            set { SetValue(value); }
-        }
+            public string MainText
+            {
+                get { return GetValue<string>(); }
+                set { SetValue(value); }
+            }
 
-        public void Init()
-        {
-            MainText = "Welcome to Page 2";
+            public string ImageFileName
+            {
+                get { return GetValue<string>(); }
+                set { SetValue(value); }
+            }
+
+            public string ButtonText
+            {
+                get { return GetValue<string>(); }
+                set { SetValue(value); }
+            }
+
+            public void Init()
+            {
+                ButtonText = "Is there a big green image?";
+                MainText = "Welcome to Page 2 (from ViewModel)";
+                ImageFileName = "BigGreenButton";
+            }
         }
     }
 }
