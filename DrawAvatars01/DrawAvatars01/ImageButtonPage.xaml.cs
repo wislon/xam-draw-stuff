64c06451f11eafc2a860a597ce7b4f94c776eeeb
John Wilson <wislon@gmail.com>
2015-11-26 11:30:29 +1100

    ui - ImageButtonPage.xaml: experimenting a bit with spacing and loading an avatar image off the interwebs

--- DrawAvatars01/DrawAvatars01/ImageButtonPage.xaml.cs
+++ DrawAvatars01/DrawAvatars01/ImageButtonPage.xaml.cs
@@ -21,22 +21,22 @@ namespace DrawAvatars01
 
             _dataPath = dataPath;
 
-            var pickupbtn = new XLabs.Forms.Controls.ImageButton()
-                            {
-                                HorizontalOptions = LayoutOptions.CenterAndExpand,
-                                WidthRequest = 200,
-                                HeightRequest = 54,
-                                ImageHeightRequest = 48,
-                                ImageWidthRequest = 48,
-                                Orientation = ImageOrientation.ImageToLeft,
-                                Text = "John Wilson",
-                                Source = "https://d2ojpxxtu63wzl.cloudfront.net/static/2bacd1af951486806f4f0c452c5549aa_9b5a49c8032bb699fd25f9d8ba090b26aa519e27ab686e66aa51457444be2699",
-                                BorderRadius = 27,
-                                BackgroundColor = Color.Silver,
-                                BorderColor = Color.Silver,
-                                BorderWidth = 2,
-                            };
-            containerLayout.Children.Add(pickupbtn);
+            //var pickupbtn = new XLabs.Forms.Controls.ImageButton()
+            //                {
+            //                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+            //                    WidthRequest = 200,
+            //                    HeightRequest = 54,
+            //                    ImageHeightRequest = 48,
+            //                    ImageWidthRequest = 48,
+            //                    Orientation = ImageOrientation.ImageToLeft,
+            //                    Text = "John Wilson",
+            //                    Source = "https://d2ojpxxtu63wzl.cloudfront.net/static/2bacd1af951486806f4f0c452c5549aa_9b5a49c8032bb699fd25f9d8ba090b26aa519e27ab686e66aa51457444be2699",
+            //                    BorderRadius = 27,
+            //                    BackgroundColor = Color.Silver,
+            //                    BorderColor = Color.Silver,
+            //                    BorderWidth = 2,
+            //                };
+            //containerLayout.Children.Add(pickupbtn);
 
         }
 
