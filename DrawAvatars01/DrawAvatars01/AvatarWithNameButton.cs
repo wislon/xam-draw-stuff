aa6d3691a28bfc0d292a86dc8374dfbf3960a234
John Wilson <wislon@gmail.com>
2015-11-26 11:33:28 +1100

    ui - DummyImageButton is now called AvatarWithNameButton

---
+++ DrawAvatars01/DrawAvatars01/AvatarWithNameButton.cs
@@ -0,0 +1,14 @@
+﻿using Splat;
+using XLabs.Forms.Controls;
+
+namespace DrawAvatars01
+{
+    public class AvatarWithNameButton : IEnableLogger
+    {
+        public AvatarWithNameButton()
+        {
+            this.Log().Debug("AvatarWithNameButton ctor");
+
+        }
+    }
+}
